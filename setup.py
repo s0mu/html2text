492e60a0f881fccf76662b2d5a3f4ce0543efce6
s0mu <66406835+s0mu@users.noreply.github.com>
2021-03-03 01:46:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name = "html2text",
-   version = "3.100.1",
+   version = "2.100.1",
    description = "Turn HTML into equivalent Markdown-structured text.",
    author = "Aaron Swartz",
    author_email = "me@aaronsw.com",
